6f1b95998dd7dbc98742fe3306ec54b3dff4f738
Jenny Bacon <jenniferbacon01@gmail.com>
2017-08-31 12:12:54 +0100

    min temp implemented

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -7,7 +7,25 @@ describe('Thermostat', function() {
 
 
   it('starts at 20 degrees', function() {
-    expect(thermostat.startTemp()).toBe('20 degrees')
+    expect(thermostat.temperature).toBe(20)
+  });
+
+  it('temperature can be increased', function() {
+    thermostat.up(1);
+    expect(thermostat.temperature).toBe(21)
+  });
+
+  it('temperature can be decreased', function() {
+    thermostat.down(1);
+    expect(thermostat.temperature).toBe(19)
+  });
+
+  it('temperature cant be decreased lower than 10', function() {
+    expect(function() {thermostat.down(11)}).toThrow('cannot decrease temperature lower than min');
+  });
+
+  it('min default temperature', function() {
+    expect(thermostat.mintemp).toBe(10)
   });
 
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,5 +1,18 @@
-var Thermostat = function(){};
-
-Thermostat.prototype.startTemp = function() {
-  return ('20 degrees');
+var Thermostat = function(temperature = 20, mintemp = 10){
+  this.temperature = temperature;
+  this.mintemp = mintemp;
+  this.up = function (number){
+    this.temperature += number;
+  };
+  this.down = function (number){
+    if (this.temperature - number < 10) {
+      throw 'cannot decrease temperature lower than min';
+    } else {
+      this.temperature -= number;
+    };
+  };
 };
+
+// Thermostat.prototype.up = function(degrees) {
+//   this.temperature += number;
+// };
